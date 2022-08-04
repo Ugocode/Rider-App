d17421e1e24f27d56ded198867c13ea4db6d2eb8
ugocode <obiohaugo5@gmail.com>
2022-08-04 18:05:54 +0100

    made little modification

--- lib/DataHandler/app_data.dart
+++ lib/DataHandler/app_data.dart
@@ -1,13 +1,20 @@
 //here we set our provider package
 
 import 'package:flutter/material.dart';
+
 import 'package:rider_app/Models/address.dart';
 
 class AppData extends ChangeNotifier {
-  Address? pickUpLocation;
+  Address? pickUpLocation, dropOffLocation;
 
-  void updatePickupLocationAddress(Address pickupAddress) {
-    pickUpLocation = pickupAddress;
+  void updatePickupLocationAddress(Address pickUpAddress) {
+    pickUpLocation = pickUpAddress;
+    notifyListeners();
+  }
+
+//for the stopping loction:
+  void updateDropOffLocationAddress(Address dropOffAddress) {
+    dropOffLocation = dropOffAddress;
     notifyListeners();
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:firebase_database/firebase_database.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'package:rider_app/Authentication/login_screen.dart';
+import 'package:rider_app/DataHandler/app_data.dart';
 import 'package:rider_app/Screens/about_us.dart';
 
 import 'Authentication/registration_screen.dart';
@@ -29,20 +31,24 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-          primaryColor: const Color.fromRGBO(115, 2, 44, 2),
-          primarySwatch: Colors.pink),
-      //creating Routes for our app
-      initialRoute: HomePage.idScreen,
-      routes: {
-        RegistrationScreen.idScreen: ((context) => const RegistrationScreen()),
-        LoginScreen.idScreen: ((context) => const LoginScreen()),
-        HomePage.idScreen: ((context) => const HomePage()),
-        AboutUs.idScreen: (context) => const AboutUs(),
-      },
+    return ChangeNotifierProvider(
+      create: ((context) => AppData()),
+      child: MaterialApp(
+        title: 'Flutter Demo',
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+            primaryColor: const Color.fromRGBO(115, 2, 44, 2),
+            primarySwatch: Colors.pink),
+        //creating Routes for our app
+        initialRoute: HomePage.idScreen,
+        routes: {
+          RegistrationScreen.idScreen: ((context) =>
+              const RegistrationScreen()),
+          LoginScreen.idScreen: ((context) => const LoginScreen()),
+          HomePage.idScreen: ((context) => const HomePage()),
+          AboutUs.idScreen: (context) => const AboutUs(),
+        },
+      ),
     );
   }
 }
